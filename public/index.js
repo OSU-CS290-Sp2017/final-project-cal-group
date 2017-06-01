95997c8d05c81d9443ee202680b280c7022423b7
forgreatness <danh1nguyen23@live.com>
2017-05-31 19:52:07 -0700

    basic

---
+++ public/index.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+document.getElementsByClassName('day').onclick = function () {
+    location.href = "file:///C:/Users/Danh/Documents/GitHub/Final%20Project/public/index2.html";
+};
