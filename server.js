c9ae675d2d735155f5ef9321c75c13e73e910eaf
kelliulep <kelliann.ulep@gmail.com>
2017-06-08 14:01:21 -1000

    added basic server stuff

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// app.get('/', function(req, res, next) {
+
+// 	res.render
+// })
