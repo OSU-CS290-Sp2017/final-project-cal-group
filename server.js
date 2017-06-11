d666c0d4483a8eaa4ba3095abd70a1a5dca0c647
kelliulep <kelliann.ulep@gmail.com>
2017-06-11 11:25:21 -1000

    added handlebars week stuff

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var path = require('path');
+var exphbs = require('express-handlebars');
+var monthData = require('./dayData');
 
 var app = express();
 var port = process.env.PORT || 3000;
+app.listen(port);
 
+//statically serve files
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// app.get('/', function(req, res, next) {
+app.engine('handlebars', exphbs({defaultLayout: 'cal_events'}));
+app.set('view engine', 'handlebars');
 
-// 	res.render
-// })
+
+//routing for week number
+//routes to a certain week to print out the events
+app.get('/week/:weekNum', function (req, res, next) {
+	var week = monthData[req.params.weekNum];
+
+	res.render('eventPage', {
+		day: week
+	});
+});
